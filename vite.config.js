9f398276a9674e24045c9789d2199d4418157d4b
HYMAKO <castanocascales@gmail.com>
2025-08-27 10:11:09 +0200

    init marketing-land

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-
 export default defineConfig({
-    plugins: [react()],
-    resolve: { alias: { '@': path.resolve(__dirname, './src') } },
-    base: '/marketing-land/',
+  plugins: [react()],
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+  // si vas a publicar en GitHub Pages como "project site", ajusta esto:
+  // base: '/landing-servicios/',
 })
